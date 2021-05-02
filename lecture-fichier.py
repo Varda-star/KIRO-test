bb544815966cfce45ee06fdd9a4c6e37725afc91
MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>
2021-05-02 20:33:34 +0200

    Create lecture-fichier.py

---
+++ lecture-fichier.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+df_nodes_nice=pd.read_csv(os.path.join("nice","nodes.csv"), sep=";")
+df_nodes_paris=pd.read_csv(os.path.join("paris","nodes.csv"), sep=";")
+df_nodes_grenoble=pd.read_csv(os.path.join("grenoble","nodes.csv"), sep=";")
+df_distance_nice=pd.read_csv(os.path.join("nice","distances.csv"), sep=";")
+df_distance_paris=pd.read_csv(os.path.join("paris","distances.csv"), sep=";")
+df_distance_grenoble=pd.read_csv(os.path.join("grenoble","distances.csv"), sep=";")
