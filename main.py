38c193f0c84bd49413a2d9b14030985ed1c0dcd0
Lembe Vardamides Emmanuel <lembe.vardamides@student-cs.fr>
2021-05-06 14:16:29 +0200

    idle

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # Ici sera écrite toutes les fnctions
+
+def indicatriceq(s):
+    if q == 'notAffected':
+        return 1
+    return 0
+
+
+# def listelement(s):
+#     elements = []
+#     for k in s["trains"]:
+#         for l in k:
+#             elements.append(l)
+#     return elements
+
+
+def indicatricej(J, s):
+    for j in J:
+        for k, l in enumerate(s):
+            if {int(k), int(l)} == {j[0], j[2]}:
+                if int(s[k]['itineraire']) == j[1] and int(s[l]['itineraire']) == j[3]:
+                    return 1
+    return 0
