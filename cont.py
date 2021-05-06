2713e28db46ba2dfbc1806580ac3f86f7db3c5e7
KarelDaccache <karel.al-daccache@ensta-paris.fr>
2021-05-06 15:08:10 +0200

    contrainte 3  sol incluse

--- cont.py
+++ cont.py
@@ -1,20 +1,20 @@
-def contraite3(solution, data, liste_contrainte):
+def contrainte3(solution, data, liste_contrainte):
 
-    list_train = [get_train[id, data] for id in solution]
+    list_train = [get_train(int(id), data) for id in solution]
 
     for i in range(len(List_train)):
-        if (list_train[i][0].get("id") in solution):
 
-            for j in range(len(list_contraite_quai)):
-                if (slolution.get("voieAQuai") in list_contraite_quai[j].get("voiesAQuaiInterdites")):
+        for j in range(len(list_contraite_quai)):
+            if (slolution[list_train[i]['id']].get("voieAQuai") in list_contraite_quai[j].get("voiesAQuaiInterdites")):
 
-                    if((List_train[i][0].get(list_train[i][0].get("id"))).get("voieAQuai") in list_contraite_quai[j].get("voiesEnLigne")):
-                        return False
+                if((List_train[i].get(list_train[i][0].get("id"))).get("voieAQuai") in list_contraite_quai[j].get("voiesEnLigne")):
+                    return False
 
-                    if (List_train[i][0].get("typesMateriels")[0] in list_contraite_quai[j].get("typesMateriels")):
-                        return False
+                if (List_train[i].get("typesMateriels")[0] in list_contraite_quai[j].get("typesMateriels")):
+                    return False
 
-                    if (List_train[i][0].get("typeCirculation") in list_contraite_quai[j].get("typesCirculation")):
-                        return False
+                if (List_train[i].get("typeCirculation") in list_contraite_quai[j].get("typesCirculation")):
+                    return False
 
-    return Bool
+    return True
+s
--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def get_itin(i, data):
         if k["id"] == i:
             return k
 
-
+def list_trains(data):
+    L=[]
+    for i in  data['trains']:
+        for j in i:
+            L.append(j)
+    return L
 def get_train(i, L):
     for k in L:
         if k["id"] == i:
@@ -48,23 +53,25 @@ def indicatricej(J, s):
     return 0
 
 
-def contraite3(solution, data, liste_contrainte):
 
-    list_train = [get_train[id, data] for id in solution]
+def contrainte3(solution, data, liste_contrainte):
 
-    for i in range(len(List_train)):
-        if (list_train[i][0].get("id") in solution):
+    list_train = [get_train(int(id), list_trains(data)) for id in solution]
+    #print (list_train)
+    for i in range(len(list_train)):
+        if (str(list_train[i].get("id")) in solution):
 
-            for j in range(len(list_contraite_quai)):
-                if (solution[list_train[i]['id']].get("voieAQuai") in list_contraite_quai[j].get("voiesAQuaiInterdites")):
+            for j in range(len(liste_contrainte)):
+               # print(solution[str(list_train[i]['id'])].get("voieAQuai"))
+                if (solution[str(list_train[i]['id'])].get("voieAQuai") in liste_contrainte[j].get("voiesAQuaiInterdites")):
 
-                    if((List_train[i].get(list_train[i].get("id"))).get("voieAQuai") in list_contraite_quai[j].get("voiesEnLigne")):
+                    if(list_train[i].get("voieEnLigne") in liste_contrainte[j].get("voiesEnLigne")):
                         return False
 
-                    if (List_train[i].get("typesMateriels") in list_contraite_quai[j].get("typesMateriels")):
+                    if (list_train[i].get("typesMateriels") in liste_contrainte[j].get("typesMateriels")):
                         return False
 
-                    if (List_train[i].get("typeCirculation") in list_contraite_quai[j].get("typesCirculation")):
+                    if (list_train[i].get("typeCirculation") in liste_contrainte[j].get("typesCirculation")):
                         return False
 
     return True
