23c4d1975fbd9b29e3a82b2a27126ac21f67d158
Lembe Vardamides Emmanuel <lembe.vardamides@student-cs.fr>
2021-05-06 14:18:54 +0200

    contrainte1

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 # Ici sera écrite toutes les fnctions
 
+def get_itin(i, data):
+    for k in data["itineraire"]:
+        if k["id"] == i:
+            return k
+
+
+def list_trains(data):
+    L = []
+    for i in data["trains"]:
+        for j in i:
+            L.append(j)
+    return L
+
+
+def get_train(i, L):
+    for k in L:
+        if k["id"] == i:
+            return k
+
+
+def contraintes1(trains, data):
+    L = list_trains(data)
+    T = True
+    for j in trains:
+        trainj = get_train(int(j), L)
+        solj = trains[j]
+        it = solj["itineraire"]
+        itinj = itin(int(it), data)
+        if (((it != "notAffected") or (qt != "notAffected")) and ((itinj["voiesAQuai"] != solj["voiesAQuai"]) or (itinj["voisEnligne"] != solj["voisEnLigne"]))):
+            T = False
+    return T
+
+
 def indicatriceq(s):
     if q == 'notAffected':
         return 1
