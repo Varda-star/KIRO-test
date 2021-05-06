f5ed760f5c195d5bac6022dcef310c326b95f403
Lembe Vardamides Emmanuel <lembe.vardamides@student-cs.fr>
2021-05-06 15:11:17 +0200

    contraintes 1,2 et 3

--- main.py
+++ main.py
@@ -20,15 +20,42 @@ def get_train(i, L):
             return k
 
 
+def in_group(idt, data):
+
+    for k in data["trains"]:
+        for i in k:
+            if i["id"] == idt:
+                return k
+
+
+def get_train_aquai(q, data):
+    for k in data["trains"]:
+        if k[0]["voieAQuai"] == q:
+            return k
+
+
 def contraintes1(trains, data):
     L = list_trains(data)
     T = True
     for j in trains:
         trainj = get_train(int(j), L)
         solj = trains[j]
-        it = solj["itineraire"]
-        itinj = itin(int(it), data)
-        if (((it != "notAffected") or (qt != "notAffected")) and ((itinj["voiesAQuai"] != solj["voiesAQuai"]) or (itinj["voisEnligne"] != solj["voisEnLigne"]))):
+        it = solj['itineraire']
+        itinj = get_itin(int(it), data)
+        if (((it != "notAffected") or (qt != "notAffected")) and ((itinj["voieAQuai"] != solj["voieAQuai"]) or (itinj["voisEnligne"] != solj["voisEnLigne"]))):
+            T = False
+    return T
+
+
+def contraintes2(trains, data):
+    L = list_trains(data)
+    T = True
+    for j in trains:
+        solj = trains[j]
+        qj = solj["voieAQuai"]
+        k = in_group(int(j), data)
+        qg = k[0]["voieAQuai"]
+        if qj != qg:
             T = False
     return T
 
